39751b96819ca361ab53d050228ac78e1e3739cc
tommyrawle <tommyrawle@hotmail.com>
2018-10-30 10:30:52 +0000

    added fetch topics func

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from '@reach/router';
 import './Nav.css';
+import * as api from '../api';
 
-const Nav = () => {
-    return (
-        <div className="nav">
-            <Link to='/'>Home</Link>
-            <div className="dropdown">
-                    <button className="dropbtn">
-                        Topics <i className="fa fa-caret-down"></i>
-                    </button>
-                    <div className="dropdown-content">
-                        <Link to='/topics/coding'>Coding</Link>
-                        <Link to='/topics/cooking'>Cooking</Link>
-                        <Link to='/topics/football'>Football</Link>
-                    </div> 
-                </div>
-        </div>
-    );
-};
+class Nav extends Component {
+    state = {
+        topics: []
+    }
+    render() {
+        return (
+            <div className="nav">
+                <Link to='/'>Home</Link>
+                <div className="dropdown">
+                        <button className="dropbtn">
+                            Topics <i className="fa fa-caret-down"></i>
+                        </button>
+                        <div className="dropdown-content">
+                            <Link to='/topics/coding'>Coding</Link>
+                            <Link to='/topics/cooking'>Cooking</Link>
+                            <Link to='/topics/football'>Football</Link>
+                        </div> 
+                    </div>
+            </div>
+        );
+    }
+    componentDidMount(){
+        this.fetchTopics()
+    }
+    componentDidUpdate(prevProps, prevState){
+        if (prevState.topics !== this.state.topics){
+            this.fetchTopics()
+        }
+    }
+    fetchTopics = () => {
+        api.getTopics()
+        .then(newTopics => {
+            this.setState({
+                topics: newTopics
+            })
+        })
+    }
+}
 
 export default Nav;
