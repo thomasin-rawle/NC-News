e5165decbe545ee8ab6d3573a97acfc647218a0e
tommyrawle <tommyrawle@hotmail.com>
2018-10-30 10:46:58 +0000

    added get topics func

--- src/api.js
+++ src/api.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://nc-news-tommy.herokuapp.com/api';
+
+export const getTopics = () => {
+    const url = `${BASE_URL}/topics`
+    return (
+        axios.get(url)
+        .then(({data}) => data.allTopics)
+    )
+}
 export const getArticlesByTopic = (topicSlug) => {
     const url = `${BASE_URL}/topics/${topicSlug}/articles`
     return (
         axios.get(url)
-        .then(({data}) => data.articles)
+        .then(({data}) => data.articlesInTopic)
     )
 }
