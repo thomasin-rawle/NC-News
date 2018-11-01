30d1cd3935a22e64dd1af5eb9f4fd0ce3782a50b
tommyrawle <tommyrawle@hotmail.com>
2018-11-01 13:53:05 +0000

    adds deleteComment functionality

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,53 +1,67 @@
 import React, { Component } from 'react';
-import * as api from '../api'
-import './Comments.css'
+import * as api from '../api';
+import './Comments.css';
 import ProfilePic from './ProfilePic';
-import Like from './Like'
+import Like from './Like';
 
 class Comments extends Component {
-    state = {
-        comments: [],
-    }
-    render() {
-        
-        return (
-        <div>
-            {this.state.comments.map(comment => {
-                return (
-                <div key={comment._id} className="comment">
-                    <div className="top-bar">
-                        <ProfilePic user={comment.created_by}/>
-                        <p>{comment.created_by.username}</p>
-                    </div>
-                    <p>{comment.body}</p>
-                    <div className="art-interactions">
-                    <Like likeCount={comment.votes}  target_id={comment._id} type={'comment'} />
-                    {comment.created_by._id === this.props.articleAuthor._id && 
-                    <button className="delete-comment" onClick={this.handleClick}>
-                        <i className="fa fa-trash-o" aria-hidden="true"></i>
-                    </button>}
-                    </div>
-                </div>
-                );
-            })}
-        </div>
+  state = {
+    comments: []
+  };
+  render() {
+    return (
+      <div>
+        {this.state.comments.map(comment => {
+          return (
+            <div key={comment._id} className="comment">
+              <div className="top-bar">
+                <ProfilePic user={comment.created_by} />
+                <p>{comment.created_by.username}</p>
+              </div>
+              <p className="body">{comment.body}</p>
+              <div className="art-interactions">
+                <Like
+                  likeCount={comment.votes}
+                  target_id={comment._id}
+                  type={'comment'}
+                />
+                {comment.created_by._id === this.props.articleAuthor._id && (
+                  <button
+                    className="delete-comment"
+                    onClick={() => {
+                      this.deleteComment(comment._id);
+                    }}
+                  >
+                    <i className="fa fa-trash-o" aria-hidden="true" />
+                  </button>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     );
   }
-  componentDidMount(){
+  componentDidMount() {
     this.fetchComments(this.props.article_id);
   }
-  componentDidUpdate(){
-      
-  }
-  fetchComments = (id) => {
-    api
-      .getComments(id)
-      .then(newComments => {
+  fetchComments = id => {
+    api.getComments(id).then(newComments => {
+        console.log(newComments)
+      this.setState({
+        comments: newComments
+      });
+    });
+  };
+  deleteComment = id => {
+    const result = window.confirm('Are you sure you want to delete this comment?')
+    if (result) {
+      api.deleteComment(id).then(newComments => {
         this.setState({
-          comments: newComments,
-
+          comments: newComments
         });
       });
+    }
   };
 }
 
