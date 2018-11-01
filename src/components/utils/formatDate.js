9042de18b09685864e212b27787b435c50c12240
tommyrawle <tommyrawle@hotmail.com>
2018-11-01 23:05:44 +0000

    improved function

--- src/components/utils/formatDate.js
+++ src/components/utils/formatDate.js
@@ -1,13 +1,17 @@
 
 const formatDate = (rawDate) => {
 const dateInMS = new Date(Date.parse(rawDate));
+
 let date = ''
 const olderThanADay = Date.now()-86400000
 if (dateInMS < olderThanADay) {
     date = dateInMS.toLocaleDateString();
 }
 else {
-    date = dateInMS.toLocaleTimeString().slice(0, -3)
+    const now = new Date(Date.now())
+    const n = now.getHours() - dateInMS.getHours()
+    console.log(n)
+    date = n === 1 ? `${n} hour ago` : `${n} hours ago`
 }
 return date
 
