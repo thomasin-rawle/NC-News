41e81183c365a9189969d0bbc93ab54ef5dda1b4
tommyrawle <tommyrawle@hotmail.com>
2018-11-01 10:35:47 +0000

    implements like functionality

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import './Articles.css'
+import * as api from '../api'
 
 class Like extends Component {
     state ={
-        liked: false
+        isliked: false,
+        voteChange: 0
     }
     render() {
         return (
             <div className="likeDiv">
-               <div className={this.state.liked? 'heart is_animating' : 'heart'} onClick={this.handleClick}></div>
-               {this.props.likeCount} Likes
+               <div className={this.state.isliked? 'heart is_animating' : 'heart'} onClick={this.handleClick}></div>
+               {this.props.likeCount + this.state.voteChange} Likes
             </div>
         );
     }
     handleClick = () => {
+        const voteChange = (!this.state.isliked) ? 1 : 0;
+        const direction = voteChange === 1 ? 'up' : 'down'
         this.setState(prevState => {
-           return {liked: !prevState.liked}
+           return {
+               isliked: !prevState.isliked,
+               voteChange
+            }
         })
+        api.updateLikeCount(this.props.article_id, direction)
     }
 }
 
