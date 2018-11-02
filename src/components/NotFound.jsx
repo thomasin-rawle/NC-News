3a624c9924398cc5206199ede0fdb749a1692364
tommyrawle <tommyrawle@hotmail.com>
2018-11-02 12:17:27 +0000

    handles errors for article not found

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NotFound.css'
+
+const NotFound = (props) => {
+    console.log(props)
+    return (
+        <div className="error">
+            {(props.path && 
+            <div>
+            <h2>Status: {props.location.state.errCode}</h2>
+            <h2>Message: {props.location.state.errMsg}</h2>
+            </div>) 
+            || <h1>Page Not Found!</h1>}
+           
+        </div>
+    );
+};
+
+export default NotFound;
